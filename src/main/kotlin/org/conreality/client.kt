66d3ba64166e37b9c851c78312b93a802c5e603e
Arto Bendiken <arto@conreality.org>
2017-08-04 17:35:27 +0200

    Implemented connecting to the database.

--- src/main/kotlin/org/conreality/client.kt
+++ src/main/kotlin/org/conreality/client.kt
@@ -3,8 +3,16 @@
 
 package org.conreality
 
-class Client() {
+import java.sql.Connection
+import java.sql.DriverManager
+import org.postgresql.jdbc.*
+
+class Client(dbName: String) {
+  var connection: Connection
+
   init {
     Class.forName("org.postgresql.Driver").newInstance()
+
+    connection = DriverManager.getConnection("jdbc:postgresql:" + dbName)
   }
 }
