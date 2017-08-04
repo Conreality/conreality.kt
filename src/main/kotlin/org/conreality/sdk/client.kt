0f4049e600db66ca76a3019853776d78d29b6ef6
Arto Bendiken <arto@conreality.org>
2017-08-04 19:14:04 +0200

    Implemented Client#login().

--- src/main/kotlin/org/conreality/sdk/client.kt
+++ src/main/kotlin/org/conreality/sdk/client.kt
@@ -3,26 +3,33 @@
 
 package org.conreality.sdk
 
+import java.util.UUID
 import java.sql.Connection
 import java.sql.DriverManager
 
 import com.zaxxer.hikari.HikariDataSource
 
-class Client(dbName: String) {
+class Client(gameID: String) {
   private val connectionPool = HikariDataSource()
-
-  //val isClosed get() = connection.isClosed // FIXME
 
   init {
     Class.forName("org.postgresql.Driver").newInstance()
-    connectionPool.setJdbcUrl("jdbc:postgresql:" + dbName)
+    connectionPool.setJdbcUrl("jdbc:postgresql:" + gameID)
+  }
+
+  fun login(agentID: String, password: String = ""): Session {
+    val agentUUID = UUID.fromString(agentID) // TODO: support names
+    return login(agentUUID, password)
+  }
+
+  fun login(agentUUID: UUID, password: String = ""): Session {
+    val connection = connectionPool.getConnection(agentUUID.toString(), password)
+    return Session(connection, agentUUID)
   }
 
   fun execute(sqlCommand: String) {
-    val connection = connectionPool.getConnection("", "")
+    val connection = connectionPool.getConnection()
     val statement = connection.createStatement()
     statement.execute(sqlCommand)
   }
-
-  fun login() {} // TODO
 }
