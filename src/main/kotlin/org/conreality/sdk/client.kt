b2f4ffbf3783e3edfa9e3ff8e86d34d4dbc2442b
Arto Bendiken <arto@conreality.org>
2017-08-04 18:53:36 +0200

    Changed Client to use connection pooling.

--- src/main/kotlin/org/conreality/sdk/client.kt
+++ src/main/kotlin/org/conreality/sdk/client.kt
@@ -5,21 +5,24 @@ package org.conreality.sdk
 
 import java.sql.Connection
 import java.sql.DriverManager
-import org.postgresql.jdbc.*
+
+import com.zaxxer.hikari.HikariDataSource
 
 class Client(dbName: String) {
-  private val connection: Connection
+  private val connectionPool = HikariDataSource()
 
-  val isClosed get() = connection.isClosed
+  //val isClosed get() = connection.isClosed // FIXME
 
   init {
     Class.forName("org.postgresql.Driver").newInstance()
-
-    connection = DriverManager.getConnection("jdbc:postgresql:" + dbName)
+    connectionPool.setJdbcUrl("jdbc:postgresql:" + dbName)
   }
 
   fun execute(sqlCommand: String) {
+    val connection = connectionPool.getConnection("", "")
     val statement = connection.createStatement()
     statement.execute(sqlCommand)
   }
+
+  fun login() {} // TODO
 }
