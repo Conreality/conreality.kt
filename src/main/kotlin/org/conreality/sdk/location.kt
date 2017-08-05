e24d314516f4fdfe9e89c8d26750d3c5a71f92e0
Arto Bendiken <arto@conreality.org>
2017-08-05 23:16:44 +0200

    Added altitude to locations.

--- src/main/kotlin/org/conreality/sdk/location.kt
+++ src/main/kotlin/org/conreality/sdk/location.kt
@@ -2,4 +2,11 @@
 
 package org.conreality.sdk
 
-data class Location(val latitude: Double, val longitude: Double)
+/**
+ * Data class for representing a pair of latitude and longitude coordinates.
+ *
+ * @property latitude  Latitude, in degrees. Range [-90, 90].
+ * @property longitude Longitude, in degrees. Range [-180, 180).
+ * @property altitude  Altitude, in meters above the WGS 84 reference ellipsoid.
+ */
+data class Location(val latitude: Double, val longitude: Double, val altitude: Double? = null)
